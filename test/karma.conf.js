e96326831349ed53917686ea814910e529d6e2d4
jp928 <piaojingtai@gmail.com>
2015-07-17 12:31:04 +1000

    Add coverage report

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,80 +4,95 @@
 // generator-karma 0.8.3
 
 module.exports = function(config) {
-  'use strict';
+    'use strict';
 
-  config.set({
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    config.set({
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '../',
+        // base path, that will be used to resolve files and exclude
+        basePath: '../',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      
+        // list of files / patterns to load in the browser
+        files: [
 
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-animate/angular-animate.js',
-      'bower_components/angular-cookies/angular-cookies.js',
-      'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-sanitize/angular-sanitize.js',
-      'bower_components/angular-touch/angular-touch.js',
-      'bower_components/angular-underscore-module/angular-underscore-module.js',
-      'bower_components/underscore/underscore.js', 
-      'bower_components/ngDialog/js/ngDialog.js',
-      
-      'app/scripts/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
-    ],
 
-    // list of files / patterns to exclude
-    exclude: [],
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/angular-animate/angular-animate.js',
+            'bower_components/angular-cookies/angular-cookies.js',
+            'bower_components/angular-resource/angular-resource.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-sanitize/angular-sanitize.js',
+            'bower_components/angular-touch/angular-touch.js',
+            'bower_components/angular-underscore-module/angular-underscore-module.js',
+            'bower_components/underscore/underscore.js',
+            'bower_components/ngDialog/js/ngDialog.js',
 
-    // web server port
-    port: 8080,
+            'app/scripts/**/*.js',
+            'test/mock/**/*.js',
+            'test/spec/**/*.js'
+        ],
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: [
-      'PhantomJS',
-      // 'Chrome'
-    ],
+        // list of files / patterns to exclude
+        exclude: [],
 
-    // Which plugins to enable
-    plugins: [
-      'karma-phantomjs-launcher',
-      // 'karma-chrome-launcher',
-      'karma-jasmine'
-    ],
+        // web server port
+        port: 8080,
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false,
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: [
+            'PhantomJS',
+            // 'Chrome'
+        ],
 
-    colors: true,
+        // Which plugins to enable
+        plugins: [
+            'karma-phantomjs-launcher',
+            // 'karma-chrome-launcher',
+            'karma-jasmine'
+        ],
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false,
 
-    // Uncomment the following lines if you are using grunt's server to run the tests
-    // proxies: {
-    //   '/': 'http://localhost:9000/'
-    // },
-    // URL root prevent conflicts with the site root
-    // urlRoot: '_karma_'
-  });
+        colors: true,
+
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+        // Uncomment the following lines if you are using grunt's server to run the tests
+        // proxies: {
+        //   '/': 'http://localhost:9000/'
+        // },
+        // URL root prevent conflicts with the site root
+        // urlRoot: '_karma_'
+
+        preprocessors: {
+            'app/scripts/**/*.js': 'coverage'
+        },
+
+        reporters: 'coverage',
+
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/',
+            file: 'coverage.txt'
+        }
+
+
+    });
+
 };
